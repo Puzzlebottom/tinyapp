1b85604ae821058fdf012ccfbb3a3915ee4ce561
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-10 15:21:31 -0400

    adds redirect at u/:id to longURL

--- express_server.js
+++ express_server.js
@@ -39,6 +39,12 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/u/:id', (req, res) => {
+  const { id } = req.params;
+  const longURL = urlDatabase[id];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
