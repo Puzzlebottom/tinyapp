49340a7987822f415b5c56d071ea3d960d7a83e9
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-11 09:29:54 -0400

    adds endpoint for /logout and displays logged in/out status in header

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const ALPHANUMERIC_CHARS = require('./constants');
 const urlDatabase = {
@@ -15,27 +17,44 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies['username'],
+    urls: urlDatabase
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies['username'],
+  };
+  res.render('urls_new', templateVars);
 });
 
 app.post('/urls', (req, res) => {
   const { longURL } = req.body;
   const id = generateRandomString(6);
   urlDatabase[id] = longURL;
-  const templateVars = { id, longURL };
+  const templateVars = { username: req.cookies['username'], id, longURL };
   res.render('urls_show', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
   const { id } = req.params;
   const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
+  const templateVars = { username: req.cookies['username'], id, longURL };
   res.render('urls_show', templateVars);
 });
 
@@ -55,12 +74,6 @@ app.get('/u/:id', (req, res) => {
 app.post('/urls/:id/delete', (req, res) => {
   const { id } = req.params;
   delete urlDatabase[id];
-  res.redirect('/urls');
-});
-
-app.post('/login', (req, res) => {
-  const { username } = req.body;
-  res.cookie('username', username);
   res.redirect('/urls');
 });
 
