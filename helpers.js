baf7919a593e19ad64596184f38c72f9dc93bb35
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-12 21:46:01 -0400

    adds /verbose/ comments to helper functions for ACs

--- helpers.js
+++ helpers.js
@@ -1,5 +1,15 @@
 const { ALPHANUMERIC_CHARS, EMAIL_VALIDATION_REGEX } = require('./constants');
 
+/**
+ * generateRandomString() is now used only to create tinyURLs.
+ * uuidv4 is used to create userIDs and visitorIDs.
+ *
+ * It takes as arguments database: object (whose keys are used for
+ * comparison to guarantee no duplicates) and a stringLength: number
+ * which controls the length of generated string. Valid characters
+ * are imported as an array in ALPHANUMERIC_CHARS.
+ */
+
 const generateRandomString = function(database, stringLength) {
   if (arguments.length > 2) throw new Error('Error: too many arguments');
   if (arguments.length < 2) throw new Error('Error: not enough arguments');
@@ -16,6 +26,14 @@ const generateRandomString = function(database, stringLength) {
   return alreadyExists ? generateRandomString(database, stringLength) : randomString;
 };
 
+/**
+ * getUserByEmail() returns a user: object whose email property
+ * matches the provided email: string.
+ *
+ * Validation of email format is supplemental to that provided by
+ * the html input type="email".
+ */
+
 const getUserByEmail = function(users, email) {
   if (arguments.length > 2) throw new Error('Error: too many arguments');
   if (arguments.length < 2) throw new Error('Error: not enough arguments');
@@ -27,10 +45,27 @@ const getUserByEmail = function(users, email) {
   }
 };
 
+/**
+ * renderUnauthorized() is a generic landing page for errors and
+ * warning messages. It provides a more seamless user experience than
+ * default browser errors.
+ *
+ * It takes a message: string, the resObject: response object, and optionally
+ * a user: object and statusCode: number. The options make the landing page
+ * more versatile, since it is sometimes shown to logged in users and sometimes
+ * to users with no credentials.
+ */
+
 const renderUnauthorized = (message, resObject, user = null, statusCode) => {
   if (statusCode) resObject.status(statusCode);
   return resObject.render('unauthorized', { message, user });
 };
+
+/**
+ * urlsForUser() returns all urls owned by a user identified by their userID: string.
+ * the urlDatabase and userDatabase: objects are included as arguments to isolate the
+ * function and make testing more straightforward.
+ */
 
 const urlsForUser = function(urlDatabase, userDatabase, userID) {
   if (arguments.length > 3) throw new Error('Error: too many arguments');
@@ -47,19 +82,29 @@ const urlsForUser = function(urlDatabase, userDatabase, userID) {
   return urls;
 };
 
+/**
+ * logVisit() is called whenever a someone (a user or otherwise) uses
+ * a TinyURL link. It selects a url object from the urlDatabase: object by
+ * matching its urlID: string.
+ *
+ * It increments the total visits and then checks whether the visitorID
+ * matches any previous users of this TinyURL. New visitors have their visitorID
+ * recorded along with a timestamp of the link's use.
+ */
+
 const logVisit = function(urlDatabase, urlID, visitorID) {
   if (arguments.length > 3) throw new Error('Error: too many arguments');
   if (arguments.length < 3) throw new Error('Error: not enough arguments');
   if (typeof urlDatabase !== 'object' || typeof urlID !== 'string' || typeof visitorID !== 'string') throw new Error('Error: invalid argument type');
 
-  const url = urlDatabase[urlID];
+  const url = urlDatabase[urlID]; // get URL
   const { visits } = url;
-  visits.total = visits.total += 1;
-  if (!visits.visitors.includes(visitorID)) {
-    visits.visitors.push(visitorID);
-    visits.unique = visits.unique += 1;
+  visits.total = visits.total += 1; // increment total visits
+  if (!visits.visitors.includes(visitorID)) { // check if visitor has used the link before
+    visits.visitors.push(visitorID); // store visitorID
+    visits.unique = visits.unique += 1; // increment unique visits
   }
-  visits.logs.unshift({ visitorID, timeStamp: new Date().toGMTString() });
+  visits.logs.unshift({ visitorID, timeStamp: new Date().toGMTString() }); // add visit to log
 };
 
 module.exports = { generateRandomString, getUserByEmail, logVisit, renderUnauthorized, urlsForUser };
